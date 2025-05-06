6c301e16d006d9a08ea6fd95487ee408d1df3517
Aikon72 <aikon@bk.ru>
2025-05-06 09:04:12 +0500

    sb/06050900

--- src/test/java/CreaphotoTest.java
+++ src/test/java/CreaphotoTest.java
@@ -24,7 +24,6 @@ public class CreaphotoTest {
         driver.manage().deleteAllCookies();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
-
     @Test
     public void testUnsuccessfulLogin() {
         driver.get("https://creaphoto.su");
