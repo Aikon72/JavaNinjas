81f9e0ab048be810e68721fcb097bd77d5117a9a
Aikon72 <aikon@bk.ru>
2025-05-06 09:12:29 +0500

    sb/06050915

--- src/test/java/CreaphotoTest.java
+++ src/test/java/CreaphotoTest.java
@@ -39,6 +39,15 @@ public class CreaphotoTest {
                 By.xpath("//*[@id=\"root\"]/main/div/div/form/div/div[6]/div/span[2]"))).getText();
         assertEquals(value, "Псевдоним или пароль - не корректны");
     }
+    @Test
+    public void testAboutUs() {
+        wait.until(ExpectedConditions.elementToBeClickable(
+                By.cssSelector(".p-menuitem-text"))).click();
+
+        String value = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("div.p-10.mb-10.bg-bright.pubtext.ck-content"))).getText();
+        assertEquals(value, "О нас");
+    }
     @AfterMethod
     public void teardown() {
         driver.quit();
